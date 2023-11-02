b91626c6ca80ca41304b96505f8fedca08f81fc6
Asadujjaman <asadujjaman15-10212@diu.edu.bd>
2023-11-02 18:35:43 +0600

    initial setup

---
+++ src/main/java/com/nitex/NitexApplication.java
@@ -0,0 +1,13 @@
+package com.nitex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NitexApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NitexApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nitex/NitexApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nitex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NitexApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
