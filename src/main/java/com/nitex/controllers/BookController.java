4fcdf7a47209995f9b374b09a269cabc942a39e3
Asadujjaman <asadujjaman15-10212@diu.edu.bd>
2023-11-02 19:09:31 +0600

    project running

---
+++ src/main/java/com/nitex/controllers/BookController.java
@@ -0,0 +1,15 @@
+package com.nitex.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/books")
+public class BookController {
+
+    @GetMapping
+    public String test() {
+        return "From Book Controller";
+    }
+}
---
+++ src/main/java/com/nitex/models/Book.java
@@ -0,0 +1,20 @@
+package com.nitex.models;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "books")
+public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    private String title;
+
+    @NotBlank
+    private String author;
+}
